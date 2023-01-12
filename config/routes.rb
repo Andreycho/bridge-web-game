ac4ab344551ee9abb53cf37c4eac1fc843a3231a
Andres <48570796+Andreycho@users.noreply.github.com>
2023-01-12 21:22:36 +0200

    [Fix/Update] HomeController fixed and URLs updated

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => { :sessions => "users/sessions" }
+
+  devise_scope :user do
+    get 'signup', to: 'devise/registrations#new'
+    get 'login', to: 'devise/sessions#new'
+    get 'logout', to: 'devise/sessions#destroy'
+    
+  end
   
   root 'home#index'
 end
